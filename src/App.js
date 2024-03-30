76359ca5373b5849efb7e8c09b771a8133f3cf7f
fitsubrat <jsubrat2000@gmail.com>
2024-03-30 17:40:58 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { useContext } from 'react';
 import './App.css';
- import Home from './Pages/Home';
- import Login from './Pages/Login';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
 import Register from './Pages/Register';
-import {BrowserRouter, Routes , Route,Navigate} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthContext } from './context/AuthContext';
+import ChatSupport from './Components/ChatSupport';
 
 function App() {
-
   const { currentUser } = useContext(AuthContext);
 
   const ProtectedRoute = ({ children }) => {
     if (!currentUser) {
       return <Navigate to="/login" />;
     }
-
-    return children
+    return children;
   };
 
   return (
@@ -30,8 +29,9 @@ function App() {
               </ProtectedRoute>
             }
           />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
+          <Route path="/ChatSupport" element={<ProtectedRoute><ChatSupport /></ProtectedRoute>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/Components/ChatSupport.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ChatSupport() {
+  // Component logic here
+  return (
+    // JSX for ChatSupport component
+    <div>Chat Support Component</div>
+  );
+}
+
+export default ChatSupport;
